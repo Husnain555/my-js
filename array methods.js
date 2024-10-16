346ec38bd863c5da87e54319670678a1369ba881
husnain555 <talk2husnain@gmail.com>
2024-10-16 16:20:40 +0500

    Mastering js

---
+++ array methods.js
@@ -0,0 +1,18 @@
+const array = [1, 2, 3, 4, 5];
+// // console.log(array);
+//  array.push(6,7,8)
+// console.log(array)
+// // push add elements to end of array if we want to begining we need to use unshift
+// array.unshift(0)
+// console.log(array)
+// const newarray=array.shift()
+// console.log(array)
+// console.log(newarray)
+// // unshift add and return lenght of the new array and shift remove elements from array and return removed elements
+array.pop()
+console.log(array)
+// pop removed last element of the array
+console.log(array.indexOf(4))
+// index of gives of index numer of elements
+console.log(array.includes(3))
+// include gives us boolean value which describe that this element was include in element
