5f44a04fab6f1543892a6ec4871dbe0a925f71a2
husnain555 <talk2husnain@gmail.com>
2024-10-17 19:22:51 +0500

    Mastering js

---
+++ get own property name.js
@@ -0,0 +1,9 @@
+const obj = {
+    a:'1',
+    b:'2',
+    c:'3',
+    d:'4',
+    e:'5',
+}
+const keys = Object.getOwnPropertyNames(obj)
+console.log(keys)
---
+++ object.entries.js
@@ -0,0 +1,8 @@
+const obj = {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: 'password',
+
+}
+let text = obj.entries(obj)
+console.log(text.name)
