82db7f84795797549f5f8a1374ec5b1e13ffdfad
husnain555 <talk2husnain@gmail.com>
2024-10-17 13:21:21 +0500

    Mastering js

---
+++ filter.js
@@ -0,0 +1,17 @@
+const array = ['husnain','babar','aliza','maida','awais','zaman']
+const newArray = array.filter((item)=>{
+    return item.indexOf('husnain')
+
+})
+// console.log(newArray)
+// console.log(array)
+const output = array.filter((item)=>{
+    return item !== 'husnain'
+
+})
+// console.log(output)
+const newArray1 = [1,2,3,4,5,6,7,8,9,10]
+const output1 = newArray1.filter((item)=>{
+    return item % 2
+})
+console.log(output1)
