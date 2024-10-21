bb5b32dd020e685fe3e3eedfc2fe262edba8cd1f
husnain555 <talk2husnain@gmail.com>
2024-10-21 11:42:33 +0500

    Mastering js

--- get own property name.js
+++ get own property name.js
@@ -6,4 +6,10 @@ const obj = {
     e:'5',
 }
 const keys = Object.getOwnPropertyNames(obj)
-console.log(keys)
+const newKeys = keys.map((item)=>{
+    return item
+})
+const newKeys1 = newKeys.map((item)=>{
+    return item.split(item.length).splice(0,1)
+})
+console.log(newKeys1)
