0c7f4cb847e79db67e405aff0010d5e101483bda
husnain555 <talk2husnain@gmail.com>
2024-10-21 14:52:26 +0500

    Mastering js

--- Implicit, Explicit, Nominal, Structuring and Duck Typing.js
+++ Implicit, Explicit, Nominal, Structuring and Duck Typing.js
@@ -1,14 +1,14 @@
-implicit type :
-in this type js automaticallsy covert on data form to another data form like numbers to string or string to numbers
-
-explicit type:
-in this user convert data types using functions
-
-Nominal types:
-in this type "explicit" data checked and convert but js not directly support this format but "Typescript" support this type
-
-Structure Type:
-in this type types was compared on structure type data .JS was a structure type langiuage in js object was compatible if they share same structure
-
-duck Type:
-in this type js comapre an object by its properties not its declaration js compare objects by its properties
+// implicit type :
+// in this type js automaticallsy covert on data form to another data form like numbers to string or string to numbers
+//
+// explicit type:
+// in this user convert data types using functions
+//
+// Nominal types:
+// in this type "explicit" data checked and convert but js not directly support this format but "Typescript" support this type
+//
+// Structure Type:
+// in this type types was compared on structure type data .JS was a structure type langiuage in js object was compatible if they share same structure
+//
+// duck Type:
+// in this type js comapre an object by its properties not its declaration js compare objects by its properties
---
+++ Lexical Enviroment.js
@@ -0,0 +1,7 @@
+function solve() {
+    console.log(b)
+}
+let b = 20
+solve();
+// So, lexical enviroment is the structure data of any enviroment  or we also call a data structure which store all the variables and function
+// is known as lexical Enviroment
