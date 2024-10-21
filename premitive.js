5a3d448866130f5d91e4711da9ce1180dd71eae2
husnain555 <talk2husnain@gmail.com>
2024-10-21 13:03:12 +0500

    Mastering js

---
+++ premitive.js
@@ -0,0 +1,10 @@
+// preitive are those data types which are not an object is called preimitive data types.
+//     for example
+// null
+// number
+// bigint
+// undeifne
+// string
+// boolean
+const new1 =foo.includes('f')
+// preimitive are data that is not an object is called preimitive and preimitive has no methods and values
