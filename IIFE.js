f084fdcc56e6b7d86cf8b93957dd438f4baa5968
husnain555 <talk2husnain@gmail.com>
2024-10-21 17:21:53 +0500

    Mastering js

---
+++ IIFE.js
@@ -0,0 +1,12 @@
+// IIFE  (Immediate invoked function Expression) or AKA Self Executing Anonymous Function
+(function (){
+    console.log(
+        'Husnain'
+    )
+}());
+
+
+
+(()=>{
+    console.log('Babar')
+})()
