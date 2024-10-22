dce528769301deea5c20c6586bbd73bf3a270ff9
husnain555 <talk2husnain@gmail.com>
2024-10-22 14:34:43 +0500

    Mastering js

---
+++ constructor function.jsx
@@ -0,0 +1,7 @@
+function Circle (radius){
+    this.radius = radius;
+}
+const newCircle =  new Circle(12)
+const newCircle1 =  new Circle(12)
+const newCircle2 =  new Circle(12)
+console.log(newCircle, newCircle1, newCircle2)
