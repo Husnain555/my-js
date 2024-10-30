7dd80dbb4c2d63b45a36b9ce12d04d8ffa44b0f4
husnain555 <talk2husnain@gmail.com>
2024-10-30 14:03:26 +0500

    Mastering js

---
+++ add sum.js
@@ -0,0 +1,12 @@
+var twoSum = function(nums, target) {
+    nums=[3,3]
+    target = 6
+    for (i= 0;i<nums.length;i++){
+        const input = target - i[0]
+        console.log(input)
+        if (map.hasOwnProperty(input)){
+            return
+        }
+    }
+
+};
